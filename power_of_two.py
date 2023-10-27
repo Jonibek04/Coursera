3a8e870e21b0a1563e80494be08adab6e33ff901
Jonibek04 <142173538+Jonibek04@users.noreply.github.com>
2023-10-27 12:22:34 +0500

    Update power_of_two.py

--- power_of_two.py
+++ power_of_two.py
@@ -4,4 +4,15 @@ def power_of_two(n):
         bit_count += (n & 1)
         n = n >> 1
     return bit_count == 1
-print(power_of_two(16))
+
+number = int(input("Enter a number: "))
+power = 0
+if power_of_two(number) is True:
+    print(f"{number} is a power of two")
+    while number != 1:
+        number /= 2
+        power += 1
+    print(f"The power is {power}")
+        
+else:
+    print(f"{number} is not a power of two")
